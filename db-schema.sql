a38bbb56654191002a55b271773eded760a12dd3
Haryz Izzudin <haryzizzudin@gmail.com>
2021-05-06 14:32:07 +0800

    Add database schema script

---
+++ db-schema.sql
@@ -0,0 +1,54 @@
+-- Table creation
+
+CREATE TABLE ConsumerUser(
+	Id INT PRIMARY KEY,
+    PhoneNumber VARCHAR(12) NOT NULL UNIQUE,
+    `Password` VARCHAR(255) NOT NULL,
+    `Name` VARCHAR(80) NOT NULL,
+    IsActive BOOL NOT NULL,
+    IsRoamingActivated BOOL NOT NULL,
+    IsIDDActivated BOOL NOT NULL,
+    EmailAddress VARCHAR(255) NOT NULL UNIQUE,
+    TotalRewardPoints INT NOT NULL
+);
+
+CREATE TABLE `Usage`(
+	Id INT PRIMARY KEY,
+    ConsumerUserId INT NOT NULL REFERENCES ConsumerUser(Id),
+    Title VARCHAR(255) NOT NULL,
+    CurrentUsage INT NOT NULL,
+    UsageLimit INT NOT NULL,
+    Unit VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE AdminUser(
+	Id INT PRIMARY KEY,
+    Username VARCHAR(255) NOT NULL UNIQUE,
+    `Password` VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Banner(
+	Id INT PRIMARY KEY,
+    PostCoverUrl TEXT NOT NULL,
+    PostTitle VARCHAR(255) NOT NULL,
+    PostShortDesc TEXT NOT NULL,
+    PostUrl TEXT NOT NULL
+);
+
+CREATE TABLE MemberLevel(
+	Id INT PRIMARY KEY,
+    `Level` INT NOT NULL,
+    LevelText VARCHAR(255) NOT NULL,
+    Threshold INT NOT NULL
+);
+
+CREATE TABLE Reward(
+	Id INT PRIMARY KEY,
+    Title VARCHAR(255) NOT NULL,
+    `Description` TEXT NOT NULL,
+    PointsRequired INT NOT NULL,
+    ExtraCashRequired BOOL NOT NULL,
+    ExtraCashAmount INT,
+    ExpiryDate TIMESTAMP NOT NULL,
+    MinimumMemberLevelId INT NOT NULL REFERENCES MemberLevel(Id)
+);
