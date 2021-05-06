3e42292dc3e49b06f097852e7056187d8736397e
Haryz Izzudin <haryzizzudin@gmail.com>
2021-05-06 15:40:04 +0800

    Add admin user entity and repository interface

---
+++ src/RedOne.Rewards.Domain/Entities/AdminUser.cs
@@ -0,0 +1,14 @@
+﻿namespace RedOne.Rewards.Domain.Entities
+{
+    public class AdminUser : BaseEntity
+    {
+        public string Username { get; }
+        public string Password { get; }
+
+        public AdminUser(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+    }
+}
---
+++ src/RedOne.Rewards.Domain/Interfaces/IAdminUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using RedOne.Rewards.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace RedOne.Rewards.Domain.Interfaces
+{
+    public interface IAdminUserRepository
+    {
+        Task<AdminUser> GetAdminUserByUsernameAsync(string username);
+    }
+}
