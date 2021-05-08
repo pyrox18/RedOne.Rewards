1685fb2d0832e4fb02cc714c179ae38594d114c7
Haryz Izzudin <haryzizzudin@gmail.com>
2021-05-08 11:35:56 +0800

    Add admin user repository implementation

---
+++ src/RedOne.Rewards.Infrastructure/Repositories/AdminUserRepository.cs
@@ -0,0 +1,29 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using RedOne.Rewards.Domain.Entities;
+using RedOne.Rewards.Domain.Interfaces;
+using System.Threading.Tasks;
+
+namespace RedOne.Rewards.Infrastructure.Repositories
+{
+    public class AdminUserRepository : BaseRepository, IAdminUserRepository
+    {
+        public AdminUserRepository(IConfiguration configuration)
+            : base(configuration)
+        {
+        }
+
+        public async Task<AdminUser> GetAdminUserByUsernameAsync(string username)
+        {
+            var query = "SELECT * FROM AdminUser WHERE Username = @Username";
+
+            using (var connection = DbConnection)
+            {
+                return await connection.QuerySingleOrDefaultAsync<AdminUser>(query, new
+                {
+                    Username = username
+                });
+            }
+        }
+    }
+}
