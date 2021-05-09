8788716ebf4bf18bafa4bed0a67d0a908550f01e
Haryz Izzudin <haryzizzudin@gmail.com>
2021-05-09 15:58:29 +0800

    Add consumer banners endpoint

---
+++ src/RedOne.Rewards.WebApi/Controllers/Consumer/BannersController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RedOne.Rewards.Application.Dtos;
+using RedOne.Rewards.Application.Interfaces;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Threading.Tasks;
+
+namespace RedOne.Rewards.WebApi.Controllers.Consumer
+{
+    public class BannersController : BaseConsumerApiController
+    {
+        private readonly IBannerService _bannerService;
+
+        public BannersController(IBannerService bannerService)
+        {
+            _bannerService = bannerService;
+        }
+
+        [HttpGet]
+        [SwaggerOperation(Tags = new[] { "Banners (Consumer)" })]
+        [SwaggerResponse(StatusCodes.Status200OK, "Returns banners", typeof(BannerDto))]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, "Not authenticated")]
+        [SwaggerResponse(StatusCodes.Status403Forbidden, "Authenticated but not a consumer user")]
+        public async Task<IActionResult> GetBanners()
+        {
+            var result = await _bannerService.GetBannersAsync();
+
+            return new JsonResult(result);
+        }
+    }
+}
