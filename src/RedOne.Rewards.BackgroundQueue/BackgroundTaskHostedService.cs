ad283d57c25462b022654aee9bda5aa19b58c208
Haryz Izzudin <haryzizzudin@gmail.com>
2021-05-10 09:34:36 +0800

    Add hosted service and queue service implementations

---
+++ src/RedOne.Rewards.BackgroundQueue/BackgroundTaskHostedService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RedOne.Rewards.BackgroundQueue
+{
+    public class BackgroundTaskHostedService : BackgroundService
+    {
+        private readonly ILogger _logger;
+        private readonly IBackgroundTaskQueue _backgroundTaskQueue;
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Starting background task service.");
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var workItem = await _backgroundTaskQueue.DequeueAsync(stoppingToken);
+
+                try
+                {
+                    await workItem(stoppingToken);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, $"Error occurred while executing {nameof(workItem)}.");
+                }
+            }
+        }
+    }
+}
---
+++ src/RedOne.Rewards.BackgroundQueue/BackgroundTaskQueue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RedOne.Rewards.BackgroundQueue
+{
+    public class BackgroundTaskQueue : IBackgroundTaskQueue
+    {
+        private readonly ConcurrentQueue<Func<CancellationToken, Task>> _workItems;
+        private readonly SemaphoreSlim _signal;
+
+        public BackgroundTaskQueue()
+        {
+            _workItems = new ConcurrentQueue<Func<CancellationToken, Task>>();
+            _signal = new SemaphoreSlim(2);
+        }
+
+        public void QueueBackgroundWorkItem(Func<CancellationToken, Task> workItem)
+        {
+            if (workItem is null)
+                throw new ArgumentNullException(nameof(workItem));
+
+            _workItems.Enqueue(workItem);
+            _signal.Release();
+        }
+
+        public async Task<Func<CancellationToken, Task>> DequeueAsync(CancellationToken cancellationToken)
+        {
+            await _signal.WaitAsync(cancellationToken);
+            _workItems.TryDequeue(out var workItem);
+
+            return workItem;
+        }
+    }
+}
